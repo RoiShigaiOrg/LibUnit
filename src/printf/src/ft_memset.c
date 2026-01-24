b0ecd1ac8c6476648bdf57eb126364481ec99112
Melchior Schmitt <melschmi@z4r11p1.42lyon.fr>
2026-01-24 11:01:19 +0100

    Add: Printf

---
+++ src/printf/src/ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melschmi <melschmi@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 12:53:56 by melschmi          #+#    #+#             */
+/*   Updated: 2025/11/10 12:56:30 by melschmi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			byte;
+	unsigned char	*mem;
+
+	mem = (unsigned char *)s;
+	byte = 0;
+	while (byte < n)
+	{
+		mem[byte] = c;
+		byte++;
+	}
+	return ((void *) s);
+}
