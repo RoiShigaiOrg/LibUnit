872891dff675dab6c560fc56d688d4627d164502
Antoine Rebillard <arebilla@z4r3p7.42lyon.fr>
2026-01-24 15:23:06 +0100

    fix tests

--- real_tests/strlen/00_launcher.c
+++ real_tests/strlen/00_launcher.c
@@ -19,7 +19,8 @@ void	launch_tests(t_list **testlist)
 int strlen_launcher(void)
 {
 	t_list *testlist;
-	
+
+	testlist = NULL;
 	load_test(&testlist, "Basic test", &basic_test);
 	load_test(&testlist, "Failed test", &failed_test);
 	launch_tests(&testlist);
--- src/unittest_list.c
+++
@@ -1,6 +0,0 @@
-#include "libunit.h"
-
-t_unittest_list	*init_unittest_list(void)
-{
-	return (NULL);
-}
