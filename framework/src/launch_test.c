015c21fd07c5c0a12dfdf03be4da857e491b978e
Melchior Schmitt <melschmi@z4r3p3.42lyon.fr>
2026-01-25 10:51:31 +0100

    Fix: Color display && added BUSERROR test

--- framework/src/launch_test.c
+++ framework/src/launch_test.c
@@ -6,7 +6,7 @@
 /*   By: melschmi <melschmi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 13:09:53 by melschmi          #+#    #+#             */
-/*   Updated: 2026/01/24 15:54:18 by melschmi         ###   ########.fr       */
+/*   Updated: 2026/01/25 10:48:21 by melschmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	test(t_unit_test *test)
 
 int	launch_test(t_list **test_list)
 {
-	int	status;
-	int	ret;
+	int		status;
+	int		ret;
 	pid_t	child_pid;
 
 	ret = 0;
@@ -28,12 +28,15 @@ int	launch_test(t_list **test_list)
 		return (-1);
 	while (*test_list)
 	{
-		if ((child_pid = fork()) == 0)
-			exit(test((*test_list)->content));
+		child_pid = fork();
+		if (child_pid == 0)
+			exit (test((*test_list)->content));
 		else
 			wait(&status);
 		if (WIFEXITED(status))
-			display_result((*test_list)->content, WIFEXITED(status));
+			display_result((*test_list)->content, WEXITSTATUS(status));
+		else if (WIFSIGNALED(status))
+			display_result((*test_list)->content, WTERMSIG(status));
 		test_list = &(*test_list)->next;
 	}
 	return (ret);
